f7a1042dcd01d03cd915456653128235fff08bd4
DethSavis <43281335+DethSavis@users.noreply.github.com>
2018-09-15 14:18:27 -0400

    first commit

---
+++ Assets/scripts/cam_script.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cam_script : MonoBehaviour {
+
+    public Transform player;
+    public Vector3 offset;
+	
+	// Update is called once per frame
+	void LateUpdate ()
+    {
+        transform.position = player.position + offset;
+    }
+}
---
+++ Assets/scripts/player_script.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_script : MonoBehaviour
+{
+
+    public float speed;
+
+    void Update()
+    {
+        //look at mouse
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, mousePos - transform.position);
+
+        //movement
+        float axisX = Input.GetAxis("Horizontal");
+        float axisY = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(axisX, axisY) * Time.deltaTime * speed, Space.World);
+    }
+}
